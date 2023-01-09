a2832967291db232f3c30a5325b6712940d1efea
trolidichvu <120279614+trolidichvu@users.noreply.github.com>
2023-01-09 09:42:17 +0700

    Update infoDoanhNghiep.js

--- core/infoDoanhNghiep.js
+++ core/infoDoanhNghiep.js
@@ -1,11 +1,15 @@
 module.exports = {
   tenDoanhNghiep: "",
+  tenQuocTe     : "",
+  tenVietTat    : "",
   maSoThue      : "",
   diaChi        : "",
   nguoiDaiDien  : "",
   dienThoai     : "",
   ngayHoatDong  : "",
   quanLyBoi     : "",
+  loaiHinhDN    : "",
+  tinhTrang     : "",
   lastUpdate    : "",
   chuaXacDinh   : ""
 }
